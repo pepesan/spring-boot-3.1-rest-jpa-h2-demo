87459377e3c3ffdb8303c1177fd0b9b6fecc2d8f
David Vaquero <pepesan@gmail.com>
2023-11-06 19:21:56 +0100

    primer commit

---
+++ src/main/java/com/cursosdedesarrollo/demothinktic/DemoThinkticApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.demothinktic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThinkticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoThinkticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursosdedesarrollo/demothinktic/DemoThinkticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.demothinktic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThinkticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
